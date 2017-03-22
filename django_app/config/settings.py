f2fa31d6d4ccfb04ef1ab4bfdf6f80f6f4c18214
LimHM <gusals3407@gmail.com>
2017-03-23 08:56:20 +0900

    csrf fail 발생하므로 SessionAuthentication 주석처리

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -57,7 +57,7 @@ ALLOWED_HOSTS = config['django']['allowed_hosts']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     )
 }
