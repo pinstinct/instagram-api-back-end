0ca9ce139740a7cdf70661fee2e32cb145bea499
LimHM <gusals3407@gmail.com>
2017-03-23 00:44:13 +0900

    DeleteToken 구현

--- django_app/member/apis/token.py
+++ django_app/member/apis/token.py
@@ -1,3 +1,6 @@
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 __all__ = (
@@ -9,5 +12,14 @@ class DeleteToken(APIView):
     """
     Post 요청이 오면 reqest.user가 인증되어 있는 경우,
     request.auth의 Token을 삭제
+
+    과제 : ObtainAuthToken
+    (http://stackoverflow.com/questions/14567586/token-authentication-for-restful-api-should-the-token-be-periodically-changed)
     """
-    pass
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        token = Token.objects.get(user=user)
+        token.delete()
+        return Response({'Delete token': token.key})
