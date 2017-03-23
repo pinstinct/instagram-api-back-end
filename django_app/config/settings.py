50e37385a4eafea133b278c3ce852e67ed7541ff
LimHM <gusals3407@gmail.com>
2017-03-23 22:33:58 +0900

    배포를 위한 설정

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -14,55 +14,83 @@ import os
 
 # DEBUG = True
 DEBUG = os.environ.get('MODE') == 'DEBUG'
+STORAGE_S3 = os.environ.get('STORAGE') == 'S3' or DEBUG is False
+DB_RDS = os.environ.get('DB') == 'RDS'
+print('DEBUG: {}'.format(DEBUG))
+print('STORAGE_S3 : {}'.format(STORAGE_S3))
+print('DB_RDS : {}'.format(DB_RDS))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Paths
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
+# Template
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# Static
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+BOWER_DIR = os.path.join(ROOT_DIR, 'bower_components')
+STATICFILES_DIRS = (
+    STATIC_DIR,
+    BOWER_DIR,
+)
+
+# Config files
 CONF_DIR = os.path.join(ROOT_DIR, '.conf-secret')
-
 CONFIG_FILE_COMMON = os.path.join(CONF_DIR, 'settings_common.json')
-CONFIG_FILE = os.path.join(CONF_DIR, 'settings_local.json' if DEBUG else 'settings_deploy.json')
-
+if DEBUG:
+    CONFIG_FILE = os.path.join(CONF_DIR, 'settings_local.json')
+else:
+    CONFIG_FILE = os.path.join(CONF_DIR, 'settings_deploy.json')
 config_common = json.loads(open(CONFIG_FILE_COMMON).read())
 config = json.loads(open(CONFIG_FILE).read())
-
+# common과 현재 사용설정 (local 또는 deploy)를 합쳐줌
 for key, key_dict in config_common.items():
     if not config.get(key):
         config[key] = {}
     for inner_key, inner_key_dict in key_dict.items():
         config[key][inner_key] = inner_key_dict
 
-# Template path
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+# AWS
+AWS_ACCESS_KEY_ID = config['aws']['access_key_id']
+AWS_SECRET_ACCESS_KEY = config['aws']['secret_access_key']
 
-# Media path
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AWS_S3_HOST = 's3.{}.amazonaws.com'.format(config['aws']['s3_region'])
+AWS_S3_SIGNATURE_VERSION = config['aws']['s3_signature_version']
+AWS_STORAGE_BUCKET_NAME = config['aws']['s3_storage_bucket_name']
+AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
-# Static path
-BOWER_DIR = os.path.join(ROOT_DIR, 'bower_components')
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    STATIC_DIR,
-    BOWER_DIR,
-)
+# Static, Media storages
+if STORAGE_S3:
+    # Static files
+    STATICFILES_STORAGE = 'config.storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    STATIC_URL = 'https://{custom_domain}/{staticfiles_location}/'.format(
+        custom_domain=AWS_S3_CUSTOM_DOMAIN,
+        staticfiles_location=STATICFILES_LOCATION,
+    )
+    # Media files
+    DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+    MEDIA_URL = 'https://{custom_domain}/{mediafiles_location}/'.format(
+        custom_domain=AWS_S3_CUSTOM_DOMAIN,
+        mediafiles_location=MEDIAFILES_LOCATION
+    )
+else:
+    STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+    STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 
-# Login URL
-LOGIN_URL = 'admin/'
-
+# Login redirect
+LOGIN_URL = '/admin/'
 SECRET_KEY = config['django']['secret_key']
 ALLOWED_HOSTS = config['django']['allowed_hosts']
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -72,9 +100,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'corsheaders',
     'rest_framework.authtoken',
     'rest_auth',
+    'corsheaders',
+    'storages',
 
     'member',
     'post',
@@ -100,15 +129,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            TEMPLATES_DIR,
+            TEMPLATE_DIR,
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                # MEDIA_URL 변수 사용
-                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -120,7 +147,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-AUTH_USER_MODEL = 'member.MyUser'
 DATABASES = {
     'default': {
         'ENGINE': config['db']['engine'],
@@ -134,6 +160,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -156,9 +183,3 @@ TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-#     'PAGE_SIZE': 5
-# }
